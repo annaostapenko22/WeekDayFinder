9fb976d4af7c23446c11a09515e4d2e7b7968abe
Anna Ostapenko <anna.ostapenko272@gmail.com>
2021-03-04 18:44:36 +0100

    first commit

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        guard let day = dateTextField.text, let month = monthTextField.text, let year = yearTextField.text  else { return }
+        
+        guard day != "", month != "", year != "" else {
+            return
+        }
+        
+             let calendar = Calendar.current
+             var dateComponents = DateComponents()
+             dateComponents.day = Int(day)
+             dateComponents.month = Int(month)
+             dateComponents.year = Int(year)
+             
+             let dateFormatter = DateFormatter()
+             dateFormatter.locale = Locale(identifier: "de_De")
+             dateFormatter.dateFormat = "EEEE"
+             
+             guard let date = calendar.date(from: dateComponents) else { return }
+             
+             let weekDay = dateFormatter.string(from: date)
+             
+             resultLabel.text = weekDay
+        
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
